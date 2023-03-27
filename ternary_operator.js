fbba1a0fb8908d1602bac0497ac9b994de32a503
irfan701 <irfanhossain701@gmail.com>
2023-03-28 05:20:31 +0600

    ternary operator

---
+++ ternary_operator.js
@@ -0,0 +1,16 @@
+
+var age=18;
+
+if(age>17){
+    console.log("Adult")
+}else{
+    console.log("Not Adult")
+}
+
+// Ternary Way
+var ageCheck=age>17?"Adult":"Not Adult";
+console.log(ageCheck)
+
+var a=5;
+var access=a>5;// (?true:false) [not needed to write if boolean]
+console.log(access)
