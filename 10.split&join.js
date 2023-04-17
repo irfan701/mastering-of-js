52f30c4df9b3badd678bb279cd85b154b76ded8f
irfan701 <irfanhossain701@gmail.com>
2023-04-18 02:42:42 +0600

    split, reverse, join

---
+++ 10.split&join.js
@@ -0,0 +1,10 @@
+
+// const str = 'I love freeCodeCamp';
+// var x=str.split(' ');
+// console.log(x)
+// // return value is ["I love freeCodeCamp"]
+
+const str = 'Ahil Yousuf';
+
+$res=str.split('').reverse().join('');
+console.log($res)
