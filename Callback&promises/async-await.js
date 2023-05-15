2dde8b25a4bd3556432645a7fdca0d3637a062e1
irfan701 <irfanhossain701@gmail.com>
2023-05-16 02:17:10 +0400

    part 1 with order maintained

---
+++ Callback&promises/async-await.js
@@ -0,0 +1,85 @@
+/*
+    Old way callback
+    Order with callback
+    promises is a inbuilt object or class
+ */
+//To make hof
+function register(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(function () {
+          console.log("Register End")
+            resolve()
+        },1000)
+    })
+
+}
+function sendEmail(){
+    return new Promise((resolve,reject)=>{
+    setTimeout(function () {
+        console.log("Email End")
+        resolve()
+    },5000)
+    })
+}
+function login(){
+    return new Promise((resolve,reject)=>{
+    setTimeout(function () {
+        console.log("Login End")
+        resolve()
+    },3000)
+    })
+}
+function getUserData(){
+    return new Promise((resolve,reject)=>{
+    setTimeout(function () {
+        console.log("Get User Data")
+        resolve()
+    },1000)
+    })
+}
+function displayUserData(){
+    return new Promise((resolve,reject)=>{
+    setTimeout(function () {
+        console.log("User Data Displayed")
+        resolve()
+    },1000)
+    })
+}
+
+
+//Nesting Function Or Callback-Hell
+// register(function () {
+//     sendEmail(function () {
+//         login(function () {
+//             getUserData(function () {
+//                 displayUserData()
+//             })
+//
+//         })
+//
+//     })
+//
+// })
+// VS
+
+
+
+// //Order Maintained
+// register().then(sendEmail).then(login).then(getUserData).then(displayUserData).catch((err)=>{
+//     console.log("Err: "+ err)
+// })
+
+//Async -await
+
+async function authenticate() {
+    await register();
+    await sendEmail();
+    await login();
+    await getUserData();
+    await displayUserData();
+
+}  //this function automatic promise return korey
+
+authenticate();
+
+console.log("Other application work!")
